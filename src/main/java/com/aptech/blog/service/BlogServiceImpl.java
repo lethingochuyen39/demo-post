f227f2dbda9aba0f2dfcac64181e283f64e02b33
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2023-04-23 13:55:41 +0700

    add ,update blog

--- src/main/java/com/aptech/blog/service/BlogServiceImpl.java
+++ src/main/java/com/aptech/blog/service/BlogServiceImpl.java
@@ -1,5 +1,6 @@
 package com.aptech.blog.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,31 +34,37 @@ public class BlogServiceImpl implements BlogService<Blog> {
 		return blog;
 	}
 
+	// tất cả các thao tác xóa được thực hiện trong một transaction
+	@Transactional
 	@Override
 	public Blog addBlog(Blog blog) {
+		List<Post> posts = new ArrayList<>(blog.getPosts());
 
-		Blog savedBlog = blogRepository.save(blog);
-		List<Post> posts = blog.getPosts();
 		if (posts != null && !posts.isEmpty()) {
 			for (Post post : posts) {
-				post.setBlog(savedBlog);
-				postRepository.save(post);
+				post.setBlog(blog);
 			}
+			blog.setPosts(posts);
 		}
+
+		Blog savedBlog = blogRepository.save(blog);
+
 		return savedBlog;
 	}
 
+	@Transactional
 	@Override
-	public Blog updateBlog(Blog blog, int id) {
-		Optional<Blog> data = blogRepository.findById(id);
-
-		Blog _blog = data.get();
-		_blog.setUrl(blog.getUrl()).setRating(blog.getRating()).setPosts(blog.getPosts());
-		return blogRepository.save(_blog);
-
+	public Blog updateBlog(Blog updatedBlog, int blogId) {
+		Blog blog = blogRepository.findById(blogId).orElse(null);
+		if (blog != null) {
+			blog.setUrl(updatedBlog.getUrl());
+			blog.setRating(updatedBlog.getRating());
+			Blog savedBlog = blogRepository.save(blog);
+			return savedBlog;
+		}
+		return null;
 	}
 
-	// tất cả các thao tác xóa được thực hiện trong một transaction
 	@Transactional
 	@Override
 	public void deleteBlogById(int id) {
@@ -67,5 +74,47 @@ public class BlogServiceImpl implements BlogService<Blog> {
 		postRepository.deleteAll(posts);
 		blogRepository.deleteById(id);
 	}
+	// @Transactional
+	// @Override
+	// public Blog updateBlog(Blog updatedBlog, int blogId) throws Exception {
+	// Blog blog = blogRepository.findById(blogId).orElse(null);
+	// if (blog == null) {
+	// return null;
+	// }
+	// // update blog's properties
+	// blog.setUrl(updatedBlog.getUrl());
+	// blog.setRating(updatedBlog.getRating());
+	// List<Post> updatedPosts = updatedBlog.getPosts();
+	// // if no post is updated, return the updated blog directly
+	// if (updatedPosts == null || updatedPosts.isEmpty()) {
+	// return blogRepository.save(blog);
+	// }
+	// // use a set to keep track of updated post IDs
+	// Set<Integer> updatedPostIds = new HashSet<>();
+	// // loop through updated posts
+	// for (Post updatedPost : updatedPosts) {
+	// Integer postId = updatedPost.getPostId();
+	// // if post ID is null or not found in the current blog's posts, create a new
+	// // post
+	// if (postId == null || !blog.containsPostWithId(postId)) {
+	// Post newPost = new Post();
+	// newPost.setTitle(updatedPost.getTitle());
+	// newPost.setContent(updatedPost.getContent());
+	// newPost.setBlog(blog);
+	// blog.getPosts().add(newPost);
+	// } else { // otherwise, update the existing post
+	// Post post = blog.getPostWithId(postId);
+	// if (post != null) {
+	// post.setTitle(updatedPost.getTitle());
+	// post.setContent(updatedPost.getContent());
+	// updatedPostIds.add(postId);
+	// }
+	// }
+	// }
+	// // remove posts that are not in the updated posts set
+	// blog.getPosts().removeIf(post -> !updatedPostIds.contains(post.getPostId()));
+	// // save and return the updated blog
+	// return blogRepository.save(blog);
+	// }
 
 }
